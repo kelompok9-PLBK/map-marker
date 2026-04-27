345e6843129c1a31af9a3468e91615b77529081c
Rahmatun Nisa <rhtn23@mhs.usk.ac.id>
2026-04-27 18:19:52 +0700

    feat: Menambahkan marker wisata di Aceh Tengah

--- kelompok-9/marker-nisa.js
+++ kelompok-9/marker-nisa.js
@@ -11,6 +11,36 @@ L.marker([4.6475, 96.8079])
   .bindPopup("<b>Pantan Terong ⛰️</b><br>Tempat terbaik menikmati panorama Kota Takengon dari ketinggian, cocok untuk melihat sunrise dan sunset.");
 
 // Bur Telege
-L.marker([4.6121, 96.8586])
+L.marker([4.6121, 96.8546])
   .addTo(map)
   .bindPopup("<b>Bur Telege 🌄</b><br>Destinasi wisata alam dengan view Danau Laut Tawar dari atas, terkenal dengan spot foto yang indah.");
+
+// Dermaga Danau Laut Tawar
+L.marker([4.6241, 96.8578])
+  .addTo(map)
+  .bindPopup("<b>Dermaga Danau Laut Tawar 🚤</b><br>Tempat santai menikmati danau sambil naik perahu dan menikmati suasana yang damai."); 
+  
+// Air Terjun Mengaya
+L.marker([4.5735, 96.9062])
+  .addTo(map)
+  .bindPopup("<b>Air Terjun Mengaya 💧</b><br>Air terjun tinggi dengan suasana alami, dikelilingi hutan hijau dan cocok untuk pecinta alam.");
+
+// Buntul Rintis
+L.marker([4.6512, 96.8201])
+  .addTo(map)
+  .bindPopup("<b>Buntul Rintis 🌄</b><br>Tempat hits dengan spot foto menarik dan pemandangan kota Takengon dari ketinggian.");
+
+// Masjid Agung Ruhama
+L.marker([4.6245, 96.8417])
+  .addTo(map)
+  .bindPopup("<b>Masjid Agung Ruhama 🕌</b><br>Masjid ikonik di pusat Kota Takengon dengan perpaduan arsitektur modern dan tradisional Gayo.");
+
+// Goa Loyang Koro
+L.marker([4.6118, 96.8702])
+  .addTo(map)
+  .bindPopup("<b>Goa Loyang Koro 🗿</b><br>Gua bersejarah di dekat Danau Laut Tawar yang menyimpan cerita masa lalu masyarakat Gayo.");
+
+// Pante Menye
+L.marker([4.5593, 96.9275])
+  .addTo(map)
+  .bindPopup("<b>Pante Menye 🌅</b><br>Tepi Danau Laut Tawar dengan pemandangan indah, cocok untuk santai sambil menikmati kuliner lokal.");  
