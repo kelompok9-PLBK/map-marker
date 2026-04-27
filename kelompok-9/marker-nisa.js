972a39b3d2dd065191409aec5894a42bbda3cfe7
Rahmatun Nisa <rhtn23@mhs.usk.ac.id>
2026-04-27 17:49:05 +0700

    feat: Menambahkan 3 marker wisata di Aceh Tengah

---
+++ kelompok-9/marker-nisa.js
@@ -0,0 +1,16 @@
+// Marker - Nisa (Aceh Tengah / Takengon)
+
+// Danau Laut Tawar
+L.marker([4.6146, 96.9229])
+  .addTo(map)
+  .bindPopup("<b>Danau Laut Tawar 🌊</b><br>Danau terbesar di Aceh yang jadi ikon Takengon, dengan pemandangan pegunungan yang tenang dan sejuk.");
+
+// Pantan Terong
+L.marker([4.6475, 96.8079])
+  .addTo(map)
+  .bindPopup("<b>Pantan Terong ⛰️</b><br>Tempat terbaik menikmati panorama Kota Takengon dari ketinggian, cocok untuk melihat sunrise dan sunset.");
+
+// Bur Telege
+L.marker([4.6121, 96.8586])
+  .addTo(map)
+  .bindPopup("<b>Bur Telege 🌄</b><br>Destinasi wisata alam dengan view Danau Laut Tawar dari atas, terkenal dengan spot foto yang indah.");
