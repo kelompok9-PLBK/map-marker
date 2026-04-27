1f89ed93a31971cb63d9b1f9e360f2f6627a5d7e
davina aura <davina23@mhs.usk.ac.id>
2026-04-27 22:42:24 +0700

    feat: Menambahkan marker wisata di Sabang

--- kelompok-9/marker-davina.js
+++ kelompok-9/marker-davina.js
@@ -9,3 +9,18 @@ L.marker([5.9059, 95.2168])
 L.marker([5.872867, 95.256719])
   .addTo(map)
   .bindPopup("<b>Pantai Iboih 🏝️</b><br>Pantai dengan air jernih dan pasir putih, cocok untuk snorkeling dan menikmati keindahan alam.");
+
+// Pulau Rubiah
+L.marker([5.883025, 95.257343])
+  .addTo(map)
+  .bindPopup("<b>Pulau Rubiah 🐠</b><br>Destinasi wisata bawah laut terkenal di Sabang dengan terumbu karang yang indah.");
+
+// Pantai Sumur Tiga
+L.marker([5.8512, 95.3306])
+  .addTo(map)
+  .bindPopup("<b>Pantai Sumur Tiga 🌊</b><br>Pantai luas dengan ombak tenang, cocok untuk bersantai dan menikmati suasana.");
+
+// Tugu I Love Sabang
+L.marker([5.8890, 95.3160])
+  .addTo(map)
+  .bindPopup("<b>Tugu I Love Sabang ❤️</b><br>Spot foto populer bagi wisatawan dengan latar tulisan ikonik Sabang.");
