39928b91cdba33de68d0762f4ba9f498ea132612
Nurul Izzati <nurulizzaa@mhs.usk.ac.id>
2026-04-27 22:34:00 +0700

    feat: tambah 10 marker wisata Banda Aceh & Aceh Besar dengan koordinat akurat dan popup deskriptif

---
+++ kelompok-9/marker-nurul.js
@@ -0,0 +1,49 @@
+// 1
+L.marker([5.5536492, 95.3152032])
+  .addTo(map)
+  .bindPopup("<b>Masjid Raya Baiturrahman</b><br>Ikon Banda Aceh dengan arsitektur megah dan penuh sejarah.");
+
+// 2
+L.marker([5.547842, 95.3126038])
+  .addTo(map)
+  .bindPopup("<b>Museum Tsunami Aceh</b><br>Tempat edukasi dan refleksi tragedi tsunami 2004.");
+
+// 3
+L.marker([5.5463858, 95.3042484])
+  .addTo(map)
+  .bindPopup("<b>PLTD Apung</b><br>Kapal besar yang terseret tsunami hingga ke tengah kota.");
+
+// 4
+L.marker([5.5749094, 95.3225019])
+  .addTo(map)
+  .bindPopup("<b>Kapal Apung Lampulo</b><br>Kapal nelayan yang tersangkut di rumah warga saat tsunami.");
+
+// 5
+L.marker([5.4807147, 95.2311612])
+  .addTo(map)
+  .bindPopup("<b>Pantai Lampuuk</b><br>Pantai pasir putih dengan pemandangan laut yang indah.");
+
+// 6
+L.marker([5.4628843, 95.229765])
+  .addTo(map)
+  .bindPopup("<b>Pantai Lhoknga</b><br>Spot surfing dengan ombak yang menantang.");
+
+// 7
+L.marker([5.6014745, 95.5327183])
+  .addTo(map)
+  .bindPopup("<b>Bukit Lamreh</b><br>View alam dari ketinggian yang keren banget.");
+
+// 8
+L.marker([5.3663583, 95.479085])
+  .addTo(map)
+  .bindPopup("<b>Waduk Keuliling</b><br>Danau buatan dengan suasana tenang dan sejuk.");
+
+// 9
+L.marker([5.561179, 95.2793707])
+  .addTo(map)
+  .bindPopup("<b>Ulee Lheue</b><br>Pantai dekat kota dengan sunset yang cantik.");
+
+// 10
+L.marker([5.5557875, 95.281597])
+  .addTo(map)
+  .bindPopup("<b>Masjid Baiturrahim Ulee Lheue</b><br>Masjid yang selamat dari tsunami.");
