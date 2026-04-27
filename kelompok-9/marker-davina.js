d5b72f64adb067e5be8ceb0be19a8378111d8ded
davina aura <davina23@mhs.usk.ac.id>
2026-04-27 22:38:09 +0700

    feat: Menambahkan marker wisata di Sabang

---
+++ kelompok-9/marker-davina.js
@@ -0,0 +1,11 @@
+// Marker - Davina (Sabang)
+
+// Titik Nol Kilometer Indonesia
+L.marker([5.9059, 95.2168])
+  .addTo(map)
+  .bindPopup("<b>Titik Nol Kilometer Indonesia 📍</b><br>Titik paling barat Indonesia yang menjadi ikon nasional dan destinasi wajib saat ke Sabang.");
+
+// Pantai Iboih
+L.marker([5.872867, 95.256719])
+  .addTo(map)
+  .bindPopup("<b>Pantai Iboih 🏝️</b><br>Pantai dengan air jernih dan pasir putih, cocok untuk snorkeling dan menikmati keindahan alam.");
